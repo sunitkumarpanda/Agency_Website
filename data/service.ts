9db8996d7e165fb4e2d8225ef227b802e1b9ce33
subhankar pahi <110842641+subhankar-pahi-2005@users.noreply.github.com>
2024-12-26 19:49:12 +0530

    services update complete ✨

--- data/service.ts
+++ data/service.ts
@@ -3,7 +3,7 @@ export const servicesData = [
         "id": 1,
         "name1": "Custom Website",
         "name2": "Development",
-        "description":"Crafting stunning, responsive websites tailored to your brand's vision",
+        "description":"Stunning, responsive websites designed for your brand.",
         "background": "h-[310px] p-12 bg-zinc-100 dark:bg-zinc-800 rounded-[45px] shadow-card border border-black dark:border-white justify-between items-center flex text-black dark:text-white",
         "arrow": "./services/ag.svg",
         "link": "./",
@@ -12,8 +12,8 @@ export const servicesData = [
     {
         "id": 2,
         "name1": "SaaS Product",
-        "name2": "Engineering",
-        "description":"End-to-end SaaS development for scalable, efficient business solutions",
+        "name2": "Development",
+        "description":"Scalable SaaS solutions for your business.",
         "background": "h-[310px] p-12 bg-lime dark:bg-lime-800 rounded-[45px] shadow-card border border-black dark:border-white justify-between items-center flex text-black dark:text-white",
         "arrow": "./services/ag.svg",
         "link": "./",
@@ -23,7 +23,7 @@ export const servicesData = [
         "id": 3,
         "name1": "SEO Optimization",
         "name2": "for Startups",
-        "description":"Quick, reliable MVPs to launch and validate your ideas faster",
+        "description":"Boost your rankings and grow your business with expert SEO.",
         "background": "h-[310px] p-12 bg-black dark:bg-gray-900 rounded-[45px] shadow-card border border-black dark:border-white justify-between items-center flex text-white",
         "arrow": "./services/ag.svg",
         "link": "./",
@@ -31,32 +31,32 @@ export const servicesData = [
     },
     {
         "id": 4,
-        "name1": "Blockchain & ",
-        "name2": "Web3 Solutions",
-        "description":"Building secure, decentralized applications to power the future of digital",
+        "name1": "Digital Marketing",
+        "name2": "for Startups",
+        "description":"Boost your brand and drive growth with expert digital marketing.",
         "background": "h-[310px] p-12 bg-zinc-100 dark:bg-zinc-800 rounded-[45px] shadow-card border border justify-between items-center flex text-black dark:text-white",
         "arrow": "./services/ag.svg",
         "link": "./",
         "image": "./services/04.svg"
     },
-    {
-        "id": 5,
-        "name1": "Backend Development in",
-        "name2": "Golang,Rust & Node",
-        "description":"Robust, high-performance backend services with cutting-edge technologies",
-        "background": "h-[310px] p-12 bg-lime dark:bg-lime-800 rounded-[45px] shadow-card border border-black dark:border-white justify-between items-center flex text-black dark:text-white",
-        "arrow": "./services/ag.svg",
-        "link": "./",
-        "image": "./services/05.svg"
-    },
-    {
-        "id": 6,
-        "name1": "API Development &",
-        "name2": "Integration",
-        "description":"Seamless API design and integration to connect and empower your software",
-        "background": "h-[310px] p-12 bg-black dark:bg-gray-900 rounded-[45px] shadow-card border border-black dark:border-white justify-between items-center flex text-white",
-        "arrow": "./services/ag.svg",
-        "link": "./",
-        "image": "./services/06.svg"
-    }
+    // {
+    //     "id": 5,
+    //     "name1": "Backend Development in",
+    //     "name2": "Golang,Rust & Node",
+    //     "description":"Robust, high-performance backend services with cutting-edge technologies",
+    //     "background": "h-[310px] p-12 bg-lime dark:bg-lime-800 rounded-[45px] shadow-card border border-black dark:border-white justify-between items-center flex text-black dark:text-white",
+    //     "arrow": "./services/ag.svg",
+    //     "link": "./",
+    //     "image": "./services/05.svg"
+    // },
+    // {
+    //     "id": 6,
+    //     "name1": "API Development &",
+    //     "name2": "Integration",
+    //     "description":"Seamless API design and integration to connect and empower your software",
+    //     "background": "h-[310px] p-12 bg-black dark:bg-gray-900 rounded-[45px] shadow-card border border-black dark:border-white justify-between items-center flex text-white",
+    //     "arrow": "./services/ag.svg",
+    //     "link": "./",
+    //     "image": "./services/06.svg"
+    // }
 ]
